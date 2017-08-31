945ec59d2849c9189f2840d701cb88c509db56d1
lovemomo <928627819@qq.com>
2017-08-31 20:20:48 +0800

    change .gitignore

---
+++ Today-News-iOS/Pods/Target Support Files/Pods-Today-News-iOS/Pods-Today-News-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Today_News_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_Today_News_iOS
+@end
---
+++ Today-News-iOS/Pods/Target Support Files/RKNotificationHub/RKNotificationHub-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RKNotificationHub : NSObject
+@end
+@implementation PodsDummy_RKNotificationHub
+@end
